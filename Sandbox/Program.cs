fcea53d9edd7c4c378c55b4be592f9c55252a1b5
Bastiaan Linders <bastiaan.linders@gmail.com>
2019-04-02 14:53:02 +0200

    Sandbox project file

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Sandbox.Abstractions;
 using Sandbox.Examples.Basic;
-using Sandbox.Examples.Chocolate;
 
 namespace Sandbox
 {
